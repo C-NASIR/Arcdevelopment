536cc799d0b13735d0b70904a88a28ade9bc056c
C-Nasir <Nasrulahi90@gmail.com>
2020-05-23 22:23:54 -0500

    Updates

---
+++ precache-manifest.50b99ee93b53386e2ec65b07df4c983f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba67b8beab72d94990bc620558d2540c",
+    "url": "/arcdevelopment/index.html"
+  },
+  {
+    "revision": "a1a37ef4fc0ff7f80d3a",
+    "url": "/arcdevelopment/static/js/2.aa40ab83.chunk.js"
+  },
+  {
+    "revision": "14c54f8d621b663941f58f815237ec03",
+    "url": "/arcdevelopment/static/js/2.aa40ab83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab94ecb6e86bdf7e3f62",
+    "url": "/arcdevelopment/static/js/main.06d2a695.chunk.js"
+  },
+  {
+    "revision": "295c35c7da8cc1819738",
+    "url": "/arcdevelopment/static/js/runtime-main.04cc6db6.js"
+  },
+  {
+    "revision": "350387f5ee9e1569dc00c4952fca7cc8",
+    "url": "/arcdevelopment/static/media/Custom Software Icon.350387f5.svg"
+  },
+  {
+    "revision": "e227d87b2dec28a248985906d655c9ed",
+    "url": "/arcdevelopment/static/media/Footer Adornment.e227d87b.svg"
+  },
+  {
+    "revision": "1894f630aea694275d0476166f03b826",
+    "url": "/arcdevelopment/static/media/background.1894f630.jpg"
+  },
+  {
+    "revision": "73caaa01e4d700f4b23e47665ecc2727",
+    "url": "/arcdevelopment/static/media/facebook.73caaa01.svg"
+  },
+  {
+    "revision": "e22079d988bd34dee9239a6288b42e7d",
+    "url": "/arcdevelopment/static/media/infoBackground.e22079d9.svg"
+  },
+  {
+    "revision": "0b117993cfdc06f6a05973ffa37ab7dc",
+    "url": "/arcdevelopment/static/media/instagram.0b117993.svg"
+  },
+  {
+    "revision": "ea0de06c0dcabac997a9b8f5605ee00b",
+    "url": "/arcdevelopment/static/media/logo.ea0de06c.svg"
+  },
+  {
+    "revision": "eb953523f70c3832f9751c1081913138",
+    "url": "/arcdevelopment/static/media/mobileBackground.eb953523.jpg"
+  },
+  {
+    "revision": "6838a9fb85f01988c85e38475ae59fb0",
+    "url": "/arcdevelopment/static/media/mobileIcon.6838a9fb.svg"
+  },
+  {
+    "revision": "c608f23f6d6a6d579e2138bd77b6d054",
+    "url": "/arcdevelopment/static/media/repeatingBackground.c608f23f.svg"
+  },
+  {
+    "revision": "435fc4bc44c1e90d5f00f4efccdb9b1d",
+    "url": "/arcdevelopment/static/media/twitter.435fc4bc.svg"
+  },
+  {
+    "revision": "6b90d7dca8c7d0d5340cc0748c6bdb3c",
+    "url": "/arcdevelopment/static/media/websiteIcon.6b90d7dc.svg"
+  }
+]);
